29b315fb4c0f0cd0dacbc532091527f6908e823e
Raigiku <raigiku@hotmail.com>
2023-05-27 18:09:33 -0500

    add code to json deserializer

--- packages/infra-common/src/json-serializer.ts
+++ packages/infra-common/src/json-serializer.ts
@@ -1,10 +1,13 @@
 export const serialize = (data: object): string =>
   JSON.stringify(data, (_, value) => (typeof value === 'bigint' ? `BIGINT::${value}` : value))
 
-export const deserialize = (json: string) =>
+export const deserialize = <T>(json: string): T =>
   JSON.parse(json, (_, value) => {
-    if (typeof value === 'string' && value.startsWith('BIGINT::')) {
-      return BigInt(value.substring(8))
-    }
+    if (typeof value === 'string' && value.startsWith('BIGINT::')) return BigInt(value.substring(8))
+    if (
+      typeof value === 'string' &&
+      /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?(?:Z|[+-]\d{2}:\d{2})$/.test(value)
+    )
+      return new Date(value)
     return value
   })
