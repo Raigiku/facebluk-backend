de822a98940b6ab54cd7681e8f1579e1cda2a73d
Raigiku <raigiku@hotmail.com>
2023-05-28 10:35:06 -0500

    move user relationship routes

--- apps/user-web-api/src/common/setup-routes.ts
+++ apps/user-web-api/src/common/setup-routes.ts
@@ -2,7 +2,6 @@ import { FastifyInstance, FastifyPluginCallback } from 'fastify'
 import { friendRequestsRoutes } from '../friend-request'
 import { postsRoutes } from '../post'
 import { userRoutes } from '../user'
-import { userRelationshipsRoutes } from '../user-relationship'
 
 export const setupRoutes = async (server: FastifyInstance) => {
   await server.register(apiRoutes, { prefix: 'api' })
@@ -11,7 +10,6 @@ export const setupRoutes = async (server: FastifyInstance) => {
 const apiRoutes: FastifyPluginCallback = async (fastify, options, done) => {
   await fastify.register(postsRoutes, { prefix: 'posts' })
   await fastify.register(friendRequestsRoutes, { prefix: 'friend-requests' })
-  await fastify.register(userRelationshipsRoutes, { prefix: 'user-relationships' })
   await fastify.register(userRoutes, { prefix: 'users' })
   done()
 }
