e55e898a871407799d960ebf3eb97220f8a132ef
Raigiku <raigiku@hotmail.com>
2023-06-13 13:34:10 -0500

    add health check endpoint

--- apps/user-web-api/src/common/setup-routes.ts
+++ apps/user-web-api/src/common/setup-routes.ts
@@ -8,8 +8,16 @@ export const setupRoutes = async (server: FastifyInstance) => {
 }
 
 const apiRoutes: FastifyPluginCallback = async (fastify, options, done) => {
+  await fastify.register(healthCheckRoute)
   await fastify.register(postsRoutes, { prefix: 'posts' })
   await fastify.register(friendRequestsRoutes, { prefix: 'friend-requests' })
   await fastify.register(userRoutes, { prefix: 'users' })
+  done()
+}
+
+const healthCheckRoute: FastifyPluginCallback = (fastify, options, done) => {
+  fastify.get('/health-check', async (request, reply) => {
+    await reply.status(200).send({ response: 'success' })
+  })
   done()
 }
